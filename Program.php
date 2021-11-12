bb4433e1e71df3c58b9981e86b7606e2fad2a39a
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2021-11-12 00:42:15 -0500

    remove Startup class

--- Program.php
+++ Program.php
@@ -2,19 +2,37 @@
 
 namespace Application;
 
+use DevNet\Core\Extensions\ApplicationBuilderExtensions;
+use DevNet\Core\Extensions\ServiceCollectionExtensions;
 use DevNet\Core\Hosting\WebHost;
-use DevNet\Core\Hosting\IWebHostBuilder;
+use DevNet\Core\Http\HttpContext;
+use DevNet\System\Async\Task;
 
 class Program
 {
     public static function main(array $args = [])
     {
-        self::createWebHostBuilder($args)->build()->run();
-    }
+        $builder = WebHost::createDefaultBuilder($args);
+        $configuration = $builder->ConfigBuilder->build();
 
-    public static function createWebHostBuilder(array $args): IWebHostBuilder
-    {
-        return WebHost::createBuilder($args)
-            ->useStartup(Startup::class);
+        $builder->configureServices(function ($services) {
+            // services
+        });
+
+        $host = $builder->build();
+
+        $host->start(function ($app) use ($configuration) {
+            if ($configuration->getValue('environment') == 'development') {
+                $app->UseExceptionHandler();
+            } else {
+                $app->UseExceptionHandler("/home/error");
+            }
+
+            $app->useRouter();
+            // middlewares
+            $app->useEndpoint(function ($routes) {
+                $routes->mapGet("/", fn (HttpContext $context): Task => $context->Response->writeAsync("Hello World!"));
+            });
+        });
     }
 }
